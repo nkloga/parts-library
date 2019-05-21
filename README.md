e082ce724004547607952a8401a605e81b5b0ce7
nkloga <n.kloga@gmail.com>
2019-05-21 16:12:36 +0300

    readme

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Parts library
 Приложение для отображения списка деталей для сборки компьютеров на складе.
 
-#Technologies in use
+# Technologies in use
 - MySQL
 - Spring boot
 - Hibernate
 
+# Issues
+- In case of issues with MySQL database, you can run it with H2 as well:
+    - change application.properties according to comments
+    - change data.sql according to comments
+    - change pom.xml 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
+
+--Use with MySQL
 USE test;
 SET GLOBAL time_zone = '-6:00';
-
 DROP TABLE IF EXISTS part;
-
 CREATE TABLE part(
     id INT(11) NOT NULL AUTO_INCREMENT,
     name VARCHAR(50),
@@ -10,7 +10,6 @@ CREATE TABLE part(
     necessity BOOLEAN,
     PRIMARY KEY (id)
 )
-
 ENGINE = InnoDB
 DEFAULT  CHARACTER SET = utf8;
 
@@ -24,3 +23,13 @@ INSERT INTO part (id, name, necessity, quantity) VALUES (7, 'Memory', 1, '31');
 INSERT INTO part (id, name, necessity, quantity) VALUES (8, 'SDD', 1, '15');
 INSERT INTO part (id, name, necessity, quantity) VALUES (9, 'GPU', 0, '5');
 
+--Use with H2
+--INSERT INTO part (id, name, necessity, quantity) VALUES (1, 'Motherboard', 1, '3');
+--INSERT INTO part (id, name, necessity, quantity) VALUES (2, 'Sound card', 0, '10');
+--INSERT INTO part (id, name, necessity, quantity) VALUES (3, 'CPU', 1, '2');
+--INSERT INTO part (id, name, necessity, quantity) VALUES (4, 'Frame lighting', 0, '12');
+--INSERT INTO part (id, name, necessity, quantity) VALUES (5, 'HDD', 0, '21');
+--INSERT INTO part (id, name, necessity, quantity) VALUES (6, 'Frame', 1, '5');
+--INSERT INTO part (id, name, necessity, quantity) VALUES (7, 'Memory', 1, '31');
+--INSERT INTO part (id, name, necessity, quantity) VALUES (8, 'SDD', 1, '15');
+--INSERT INTO part (id, name, necessity, quantity) VALUES (9, 'GPU', 0, '5');
